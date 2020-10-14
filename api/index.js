4a1f706adfeeae991c3baa863c48f669afad8ebc
arieltecay <arieltecay@gmail.com>
2020-10-14 16:53:11 -0300

    update

--- api/index.js
+++ api/index.js
@@ -1,23 +1,29 @@
-/* 
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app');
-const { conn } = require('./src/database.js');
+const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
-}); */
+conn.sync({ force: true }).then(() => {
+});
 
-const express = require('express');
-const server = require('./src/app');
-
-//Middlewares
-server.use(express.json());
-server.use(express.urlencoded({extended:false})) //Funcion que me permita procesar datos desde un formulario
-
-//routes
-server.use(require('./src/routes/index'))
-
-server.listen(3001)
-console.log("%s listening at 3001");
+server.listen(3001, () => {
+  console.log('%s listening at 3001'); // eslint-disable-line no-console
+});
--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,17 +1,17 @@
 const server = require("express").Router();
-const { User } = require('../database')
+const { User } = require('../db')
 
 server.get("/", async (req, res) => {
-  // res.send("Funcionamiento general del sistema para ariel tecay")
-  server.get("/", (req, res) => {
+  res.send("Funcionamiento general del sistema para ariel tecay")
+/*   server.get("/", (req, res) => {
     User.findAll({})
       .then((users) => { res.send(users); })
       .catch((err) => res.status(400).json({ err }));
-  });
+  }); */
 });
 
 //ruta para crear usuario
-server.post("/newUser", (req, res) => {
+server.post("/", (req, res) => {
   User.create({
     name: req.body.name,
     lastName: req.body.lastName,
@@ -21,7 +21,7 @@ server.post("/newUser", (req, res) => {
     role: req.body.role
   }).then(user => {
     res.status(200).send("Agregado Satisfactoriamente" + user)
-  }).catch(err => res.send(err + "Mal funcionamiento general "));
+  }).catch(err => res.send(err));
 });
 
 module.exports = server;
